893c8b9073f4ff6a34fdc9c9adedc95469ec32ec
Severi Vidnäs <severi.vidnas@gmail.com>
2021-07-26 12:01:33 +0300

    Formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,14 @@ const IndexPage: React.FC<PageProps<{}>> = () => {
   return (
     <Layout>
       <SEO />
-      <Typography variant="subtitle1" component="h6" color="error" align="center">Work in progress</Typography>
+      <Typography
+        variant="subtitle1"
+        component="h6"
+        color="error"
+        align="center"
+      >
+        Work in progress
+      </Typography>
       <Contact />
     </Layout>
   );
