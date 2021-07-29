6bdb8304b1bef2d281f1fd1a2ad229c2ae4564ac
Severi Vidnäs <severi.vidnas@gmail.com>
2021-07-29 12:57:56 +0300

    More components added to the provider

--- plugins/gatsby-plugin-mui-mdx-provider/MuiComponentsProvider.tsx
+++ plugins/gatsby-plugin-mui-mdx-provider/MuiComponentsProvider.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import { Typography, Link } from '@material-ui/core';
-
-const muiParagraph = (props) => <Typography {...props} />;
-const muiH1 = (props) => <Typography varianth='h1' {...props} />;
-const muiH2 = (props) => <Typography varianth='h2' {...props} />;
-const muiH3 = (props) => <Typography varianth='h3' {...props} />;
-const muiH4 = (props) => <Typography varianth='h4' {...props} />;
-const muiH5 = (props) => <Typography varianth='h5' {...props} />;
-const muiH6 = (props) => <Typography varianth='h6' {...props} />;
-const muiLink = (props) => <Link {...props} />;
+import { Typography, Link, Divider, List, ListItem } from '@material-ui/core';
 
 const components = {
-  p: muiParagraph,
-  h1: muiH1,
-  h2: muiH2,
-  h3: muiH3,
-  h4: muiH4,
-  h5: muiH5,
-  h6: muiH6,
-  a: muiLink,
+  p: (props) => <Typography {...props} />,
+  h1: (props) => <Typography varianth='h1' {...props} />,
+  h2: (props) => <Typography varianth='h2' {...props} />,
+  h3: (props) => <Typography varianth='h3' {...props} />,
+  h4: (props) => <Typography varianth='h4' {...props} />,
+  h5: (props) => <Typography varianth='h5' {...props} />,
+  h6: (props) => <Typography varianth='h6' {...props} />,
+  thematicBreak: (props) => <Divider {...props} />,
+  blockquote: (props) => <Typography variant='body2' component='blockquote' {...props} />,
+  a: (props) => <Link {...props} />,
+  ul: (props) => <List {...props} />,
+  ol: (props) => <List component='ol' {...props} />,
+  li: (props) => <ListItem {...props} />,
 };
 
 const MuiComponentProvider = ({ children }) => {
