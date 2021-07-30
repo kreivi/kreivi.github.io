d7d5acae8ea28010c7f24beff7ffe6c0c76e773b
Severi Vidnäs <severi.vidnas@gmail.com>
2021-07-30 12:48:38 +0300

    Dependencies updated

--- src/components/About/theme.tsx
+++ src/components/About/theme.tsx
@@ -1,6 +1,6 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-const DEFAULT_THEME = createMuiTheme();
-const theme = createMuiTheme({
+import { createTheme } from '@material-ui/core/styles';
+const DEFAULT_THEME = createTheme();
+const theme = createTheme({
   palette: {
     type: 'light',
   },
