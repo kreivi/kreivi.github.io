621f10e8bd4671f515dadfba8312fcc4a8dc606b
Severi Vidnäs <severi.vidnas@gmail.com>
2021-07-30 11:57:04 +0300

    Added rest of the mdx->mui components

--- plugins/gatsby-plugin-mui-mdx-provider/MuiComponentsProvider.tsx
+++ plugins/gatsby-plugin-mui-mdx-provider/MuiComponentsProvider.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import { Typography, Link, Divider, List, ListItem } from '@material-ui/core';
+import { Typography, Link, Divider, List, ListItem, Table, TableRow, TableCell } from '@material-ui/core';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const components = {
   p: (props) => <Typography {...props} />,
@@ -12,10 +13,21 @@ const components = {
   h6: (props) => <Typography varianth='h6' {...props} />,
   thematicBreak: (props) => <Divider {...props} />,
   blockquote: (props) => <Typography variant='body2' component='blockquote' {...props} />,
-  a: (props) => <Link {...props} />,
   ul: (props) => <List {...props} />,
   ol: (props) => <List component='ol' {...props} />,
   li: (props) => <ListItem {...props} />,
+  table: (props) => <Table {...props} />,
+  tr: (props) => <TableRow {...props} />,
+  th: (props) => <TableCell component='thead' {...props} />,
+  td: (props) => <TableCell {...props} />,
+  pre: (props) => <Typography component='pre' {...props} />,
+  code: (props) => <Typography component='code' {...props} />,
+  em: (props) => <Typography component='em' {...props} />,
+  strong: (props) => <Typography component='strong' {...props} />,
+  delete: (props) => <Typography component='del' {...props} />,
+  inlineCode: (props) => <Typography component='code' {...props} />,
+  hr: (props) => <Divider component='hr' {...props} />,
+  a: (props) => <Link {...props} />,
 };
 
 const MuiComponentProvider = ({ children }) => {
