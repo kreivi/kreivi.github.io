948af601e6248e6d5a37a4245a3530c97ecfbcae
Severi Vidnäs <severi.vidnas@gmail.com>
2022-01-27 21:04:09 +0200

    Independent links to game page, gameplay video & source codes

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import { makeStyles, createStyles, Theme, Typography, ImageList, ImageListItem, Paper, Box } from '@material-ui/core';
+import {
+  makeStyles,
+  createStyles,
+  Theme,
+  Typography,
+  ImageList,
+  ImageListItem,
+  Paper,
+  Box,
+  Tooltip,
+} from '@material-ui/core';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import LinkIconButton from '../LinkIconButton';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -34,7 +47,9 @@ export type GameProps = {
   title?: string;
   creationDate?: string;
   shortDescription?: string;
-  screenshots?: string[];
+  gameLink?: string;
+  gameplayVideoLink?: string;
+  sourceCodeLink?: string;
   children?: string;
 };
 
@@ -43,7 +58,15 @@ export type GameProps = {
  * @param props game properties
  * @returns game element
  */
-const Game: React.FC<GameProps> = ({ title, creationDate, shortDescription, screenshots, children }) => {
+const Game: React.FC<GameProps> = ({
+  title,
+  creationDate,
+  shortDescription,
+  gameLink,
+  gameplayVideoLink,
+  sourceCodeLink,
+  children,
+}) => {
   const classes = useStyles();
   return (
     <Paper className={classes.root} component='article'>
@@ -53,15 +76,29 @@ const Game: React.FC<GameProps> = ({ title, creationDate, shortDescription, scre
       <Typography className={classes.shortDescription} variant='body2' align='center' component='em'>
         {shortDescription}
       </Typography>
-      <MDXRenderer frontmatter={{ title, creationDate, shortDescription, screenshots }}>{children}</MDXRenderer>
+      {gameLink && (
+        <LinkIconButton href={gameLink} aria-label='Link to game page' size='medium'>
+          <Tooltip title='Itch page'>
+            <StaticImage src='https://static.itch.io/images/badge-color.svg' alt='Itch page' />
+          </Tooltip>
+        </LinkIconButton>
+      )}
+      {gameplayVideoLink && (
+        <LinkIconButton href={gameplayVideoLink} aria-label='Link to gameplay video' size='medium'>
+          <Tooltip title='YouTube video'>
+            <StaticImage src='../../../static/assets/images/youtube_social_icon_red.png' alt='YouTube video' />
+          </Tooltip>
+        </LinkIconButton>
+      )}
+      {sourceCodeLink && (
+        <LinkIconButton href={gameplayVideoLink} aria-label='Link to game source code' size='medium'>
+          <Tooltip title='GitHub repository'>
+            <StaticImage src='../../../static/assets/images/static/GitHub_Logo_White.png' alt='GitHub repository' />
+          </Tooltip>
+        </LinkIconButton>
+      )}
+      <MDXRenderer frontmatter={{ title, creationDate, shortDescription }}>{children}</MDXRenderer>
       <div className={classes.margin} />
-      <ImageList className={classes.imageList} rowHeight={300}>
-        {screenshots?.map((screenshot, index) => (
-          <ImageListItem key={index}>
-            <img className={classes.img} src={screenshot} alt={screenshot} />
-          </ImageListItem>
-        ))}
-      </ImageList>
     </Paper>
   );
 };
