68b4f991d9b3356b0f51c20e493e18b8ee85d409
Severi Vidnäs <severi.vidnas@gmail.com>
2023-03-07 18:31:43 +0200

    WIP updating gatsby v4->v5

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Typography, Paper, Tooltip, List, Box } from '@mui/material';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { StaticImage } from 'gatsby-plugin-image';
+import React from "react";
+import { Typography, Paper, Tooltip, List, Box } from "@mui/material";
+import { StaticImage } from "gatsby-plugin-image";
 
-import LinkIconButton from '../LinkIconButton';
+import LinkIconButton from "../LinkIconButton";
+import { MDXProvider } from "@mdx-js/react";
 
 /**
  * Game properties.
@@ -35,53 +35,70 @@ const Game: React.FC<GameProps> = ({
   return (
     <Paper
       sx={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
         padding: 2,
       }}
-      component='article'
+      component="article"
     >
-      <Typography variant='h3' align='center'>
+      <Typography variant="h3" align="center">
         {title}
       </Typography>
-      <Typography sx={{ marginBottom: 2 }} variant='body2' align='center' component='em'>
+      <Typography
+        sx={{ marginBottom: 2 }}
+        variant="body2"
+        align="center"
+        component="em"
+      >
         {shortDescription}
       </Typography>
-      <MDXRenderer frontmatter={{ title, creationDate, shortDescription }}>{children}</MDXRenderer>
+      {children}
       <List
         sx={{
-          alignSelf: 'center',
-          justifyContent: 'center',
-          transform: 'translateZ(0)',
+          alignSelf: "center",
+          justifyContent: "center",
+          transform: "translateZ(0)",
         }}
       >
         {gameLink && (
-          <LinkIconButton href={gameLink} aria-label='Link to game page' size='small'>
+          <LinkIconButton
+            href={gameLink}
+            aria-label="Link to game page"
+            size="small"
+          >
             <StaticImage
-              src='../../../static/assets/images/itch-badge-color.png'
-              alt='Itch page'
-              placeholder='blurred'
+              src="../../../static/assets/images/itch-badge-color.png"
+              alt="Itch page"
+              placeholder="blurred"
               width={75}
             />
           </LinkIconButton>
         )}
         {gameplayVideoLink && (
-          <LinkIconButton href={gameplayVideoLink} aria-label='Link to gameplay video' size='small'>
+          <LinkIconButton
+            href={gameplayVideoLink}
+            aria-label="Link to gameplay video"
+            size="small"
+          >
             <StaticImage
-              src='../../../static/assets/images/youtube_social_icon_red.png'
-              alt='YouTube video'
-              placeholder='blurred'
+              src="../../../static/assets/images/youtube_social_icon_red.png"
+              alt="YouTube video"
+              placeholder="blurred"
               width={30}
             />
           </LinkIconButton>
         )}
         {sourceCodeLink && (
-          <LinkIconButton href={sourceCodeLink} aria-label='Link to game source code' size='small'>
+          <LinkIconButton
+            href={sourceCodeLink}
+            aria-label="Link to game source code"
+            size="small"
+          >
             <StaticImage
-              src='../../../static/assets/images/GitHub_Logo_White.png'
-              alt='GitHub repository'
-              placeholder='blurred'
+              src="../../../static/assets/images/GitHub_Logo_White.png"
+              alt="GitHub repository"
+              placeholder="blurred"
               width={50}
             />
           </LinkIconButton>
